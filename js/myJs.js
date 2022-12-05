6167279820cc022c22bceea6ca31d65bb4326b7e
thanhtungmk <115176273+thanhtungmk@users.noreply.github.com>
2022-12-05 22:20:05 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.youtube.com/watch?v=fRh_vgS2dFE";
+            window.location = "https://www.youtube.com/watch?v=Wu8NeFXaoOc";
           },
         });
       }
